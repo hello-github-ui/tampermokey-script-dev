8e957b67e305840f6de27a983718955eb926f688
hello-github-ui <util.you.come@gmail.com>
2024-06-07 10:56:12 +0800

    修改vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path"
 
 // https://vitejs.dev/config/
 // vite 的配置文件
+// 自定义构建过程
 export default defineConfig({
+    base: './',
     plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src")
+        }
+    },
+    build: {
+        outDir: './docs',
+    }
 });
